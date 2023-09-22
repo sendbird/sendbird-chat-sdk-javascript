1b80cdff68b8f64b5fe23fe578d3829fd655f77f
Chris Heo <chrisallo.h@gmail.com>
2023-09-22 10:40:29 -0700

    v4.9.12

--- cjs/lib/__bundle-0acc0d7b.js
+++
@@ -1,1 +0,0 @@
-"use strict";var s=require("./__bundle-fc8b8d13.js");class t extends s.User{constructor(s,t){var e;super(s,t),this.isMuted=!1,this.isMuted=null!==(e=t.is_muted)&&void 0!==e&&e}}exports.Participant=t;
---
+++ cjs/lib/__bundle-2a594925.js
@@ -0,0 +1,1 @@
+"use strict";var e=require("./__bundle-5d8ae943.js");class s extends e.BaseCommand{}exports.NotificationCollectionRefreshCommand=s,exports.getFeedChannelIndexBy=e=>["-lastMessageUpdatedAt","-createdAt","syncIndex"];

1f93e5dd9206009e22a9c87cf78eb1260792d309
sendbird-sdk-deployment <sha.sdk_deployment@sendbird.com>
2024-04-19 00:51:40 +0000

    Release - 4.12.3

---
+++ cjs/lib/__bundle-5b1d50ac.cjs
@@ -0,0 +1,1 @@
+var e=require("./__bundle-117a6f08.cjs");class a extends e.BaseCommand{}exports.NotificationCollectionRefreshCommand=a,exports.getFeedChannelIndexBy=e=>["-lastMessageUpdatedAt","-createdAt","syncIndex"];
